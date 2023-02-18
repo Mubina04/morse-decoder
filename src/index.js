6b9fab3fbae5074297dd079cdbd77c31b813b5d8
Mubinabegim <mubinabegimxayrullayeva@gmail.com>
2023-02-18 09:33:42 +0500

    done

--- src/index.js
+++ src/index.js
@@ -39,6 +39,34 @@ const MORSE_TABLE = {
 
 function decode(expr) {
     // write your solution here
+    let words = expr.split("**********");
+  let encoded = [];
+  for (let word = 0; word < words.length; word++) {
+    let qwerty = [];
+    for (let a = 0; a < words[word].length; a += 10) {
+      let symbol = words[word].slice(a, a + 10);
+      let objKey = "";
+      for (let b = 0; b < symbol.length; b += 2) {
+        let s = symbol.slice(b, b + 2);
+        if (s === "11") {
+          objKey += "-";
+        } else if (s === "10") {
+          objKey += ".";
+        }
+      }
+      qwerty.push(MORSE_TABLE[objKey]);
+    }
+    encoded.push(qwerty);
+  }
+  let resArr = [];
+  encoded.forEach((a) => {
+    let word = "";
+    a.forEach((b) => {
+      word += b;
+    });
+    resArr.push(word);
+  });
+  return resArr.join(" ");
 }
 
 module.exports = {
